6ef0c8a44cc2debc736650cff34506c144f1ef23
mannyavila <mannyavila90@gmail.com>
2023-06-21 16:17:23 -0400

    Action para el formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,10 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Form } from 'react-router-dom'
 import Formulario from '../components/Formulario'
+
+export function action() {
+  console.log('presssssss')
+  return null
+}
 
 const NuevoCliente = () => {
 
@@ -20,14 +25,14 @@ const NuevoCliente = () => {
       </div>
 
       <div className="bg-white shadow rounded-lg md:w-3/4 mx-auto px-5 py-10 mt-20">
-        <form action="">
+        <Form method='post'>
           <Formulario />
           <input 
             type="submit" 
             className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg rounded-md' 
             value='Registrar Cliente'
           />
-        </form>
+        </Form>
       </div>
     </>
   )
