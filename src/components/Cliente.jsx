3b3d4dc0c42c102c73ecf5b54022399a673d65aa
mannyavila <mannyavila90@gmail.com>
2023-06-21 00:30:18 -0400

    Iterar sobre clientes y mostrar la informacion

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,13 @@
+
+const Cliente = ({cliente}) => {
+
+    const {nombre, empresa, email, telefono, id} = cliente
+
+  return (
+    <tr>
+        <td className="p-5">{nombre}</td>
+    </tr>
+  )
+}
+
+export default Cliente
