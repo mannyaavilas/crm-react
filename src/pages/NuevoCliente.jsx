d922c0e7257383de692dafb1894a7b6226f5e109
mannyavila <mannyavila90@gmail.com>
2023-06-26 17:55:03 -0400

    Mas validaviones con useLoaderData y useActionData

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,19 +1,41 @@
-import { useNavigate, Form } from 'react-router-dom'
+import { useNavigate, Form, useActionData } from 'react-router-dom'
 import Formulario from '../components/Formulario'
+import Error from '../components/Error'
 
-export function action() {
-  console.log('presssssss')
-  return null
+export async function action({request}) {
+  const formData = await request.formData()
+
+  const datos = Object.fromEntries(formData)
+
+  const email = formData.get('email')
+
+  // Validacion
+  const errores = []
+  if (Object.values(datos).includes('')) {
+    errores.push('Todos los campos son obligatorios')
+  }
+
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)){
+    errores.push('El email no es válido')
+  }
+
+  // Retornar datos si hay errores
+  if (Object.keys(errores).length) {
+    return errores
+  }
 }
 
 const NuevoCliente = () => {
+
+  const errores = useActionData()
 
   const navigate = useNavigate()
 
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
-      <p className="mt-3">LLlena todos los campos para registrar un nuevo cliente</p>
+      <p className="mt-3">Llena todos los campos para registrar un nuevo cliente</p>
 
       <div className="flex justify-end">
         <button 
@@ -25,7 +47,11 @@ const NuevoCliente = () => {
       </div>
 
       <div className="bg-white shadow rounded-lg md:w-3/4 mx-auto px-5 py-10 mt-20">
-        <Form method='post'>
+        {errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error>)}
+        <Form 
+          method='post'
+          noValidate
+        >
           <Formulario />
           <input 
             type="submit" 
