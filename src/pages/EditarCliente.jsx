36ced5baeb37d98f68bba3d8f59b5ee6e7d36062
mannyavila <mannyavila90@gmail.com>
2023-07-05 17:21:53 -0400

    Verificar que el cliente exista

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,7 +1,15 @@
+import { obtenerCliente } from '../data/clientes'
+
 export async function loader({params}){
-console.log(params)
-return {}
-}
+    const cliente = await obtenerCliente(params.clienteId)
+    if (Object.values(cliente).length === 0) {
+        throw new Response('', {
+            status: 404,
+            statusText: 'No hay Resultados'
+        })
+    }
+    return cliente
+ }
 
 const EditarCliente = () => {
   return (
